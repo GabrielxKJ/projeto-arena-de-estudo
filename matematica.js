a135293b3d5ee670d29e3162df2f224e7354cc0f
User <gabrielkleber006@gmail.com>
2023-06-30 11:38:29 -0300

    alteracoes-de-inicio-de-projeto

---
+++ matematica.js
@@ -0,0 +1,15 @@
+// let arrayN = [1, 2, 3, 4, 5 , 6 , 7, 8, 9, 10];
+
+function multiplica() {
+    let result;
+    for(let i = 1; i <= 10; i++) {
+        eachNum = i;
+        for(ii = 1; ii <= 10; ii++){
+            result = eachNum * ii;
+            console.log(result)
+        }
+    };
+    return result;
+}
+
+multiplica()
